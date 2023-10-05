0c9c9aa19afec8dcd6de0138ff93bf807a00cd69
hmkuklinski <hmkuklinski@gmail.com>
2023-10-05 16:22:24 -0400

    added let_const.js
    activity 6

---
+++ let_const.js
@@ -0,0 +1,12 @@
+const strayKids = 8;
+
+let maknae= 22;
+let imFoive= 5;
+let chanChildren= 7;
+let minsung= "forever";
+
+console.log(strayKids);
+console.log (imFoive);
+console.log(chanChildren);
+console.log(minsung);
+console.log(maknae);
