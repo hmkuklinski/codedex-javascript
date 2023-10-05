fa084e70218891003e69c920d414b3b36fa30cf6
hmkuklinski <hmkuklinski@gmail.com>
2023-10-05 12:46:45 -0400

    added good_morning.js
    activity 12

---
+++ good_morning.js
@@ -0,0 +1,13 @@
+// Write code below 💖
+
+var hour = 12.50;
+
+if (hour< 12) {
+  console.log("Good Morning");
+  console.log("\nTo Do: \n1)Brush your Teeth \n2)Brush your hair \n3)Get Dressed \n4)Pack your bag \n5)Drive to Starbucks \n6)Get a drink \n7)Catch the bus");
+}
+
+else {
+  console.log("Good Afternoon. \nHave a great day!");
+  console.log("\nTo Do: \n1) Take notes for Java class \n2) Take notes for Discrete Mathematics \n3) Complete Python assignment \n4) Eat dinner");
+}
