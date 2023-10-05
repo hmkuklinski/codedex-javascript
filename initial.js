1f464bcfcc7a43aa02d7c39d6384c2fa6dd9957b
hmkuklinski <hmkuklinski@gmail.com>
2023-10-05 17:19:02 -0400

    added intial.js
    activity 4

---
+++ initial.js
@@ -0,0 +1,8 @@
+//my fun fact is that i can speak Korean
+console.log ("H   H  K    K");
+console.log ("H   H  K   K ");
+console.log ("H   H  K  K");
+console.log ("HHHHH  KKK");
+console.log ("H   H  K  K");
+console.log ("H   H  K    K ");
+console.log ('h   H  K     K');
