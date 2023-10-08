6b82eca78439dd0721d577580a7b5b369a6576f6
hmkuklinski <hmkuklinski@gmail.com>
2023-10-07 20:27:12 -0400

    added even the odds
    nested loops, continues, break; activity 21

---
+++ even_the_odds.js
@@ -0,0 +1,12 @@
+for (let i=0; i<50; i++){
+    if (i%2 != 0){
+      continue;
+    }
+    else{
+      console.log(i);
+      if (i==42){
+        break;
+      }
+    }
+  
+  }
--- luckyNumber.js
+++ luckyNumber.js
@@ -3,8 +3,8 @@ const luckyNumber = 3;
 let guess = Math.floor(Math.random()*10)+1;
 
 while (guess !=luckyNumber){
-  let guess = Math.floor(Math.random()*10)+1;
   console.log("Nope, it isn't " + guess);
+  guess = Math.floor(Math.random()*10)+1;
 }
 
 console.log("My lucky number is " + luckyNumber);
