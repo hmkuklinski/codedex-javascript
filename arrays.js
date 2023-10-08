77e75a21ff68a9022b71a0e35102124f2fae94e2
hmkuklinski <hmkuklinski@gmail.com>
2023-10-08 13:35:46 -0400

    added arrays.js
    activity 23

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const myNYCDestinations = [
+    "🗽 Manhattan - Washington Square Park",
+    "🏟️ The Bronx - Yankee Stadium",
+    "🎡 Brooklyn - Coney Island",
+    "✈️ Queens - Astoria Park",
+    "🌉 Staten Island - Historic Richmond Town"
+    ];
+    
+for (let i=0; i<myNYCDestinations.length; i++){ //added a for loop to print the array :)
+    console.log(myNYCDestinations[i]);
+}
+    
