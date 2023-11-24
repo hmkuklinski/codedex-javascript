664d217485d1704ae09599c19bcd816c4ec16d6c
hmkuklinski <hmkuklinski@gmail.com>
2023-11-24 16:07:51 -0500

    lesson 41
    drake meme

---
+++ hotline-bling/script.js
@@ -0,0 +1,8 @@
+// Write code below 💖
+
+const drakePicTwo= document.getElementById('drake-pic-2');
+const heading = document.getElementById('heading-2');
+
+drakePicTwo.src= "./drake2.png";
+heading.innerHTML= "Pie Eating Contest";
+
