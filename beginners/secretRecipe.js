53467477cf1c408e3af5ee859b7f6fd990be457e
hmkuklinski <hmkuklinski@gmail.com>
2024-10-30 16:23:33 -0400

    added lesson 4- secret recipe

---
+++ beginners/secretRecipe.js
@@ -0,0 +1,6 @@
+// Write code below 💖
+
+console.log("Ingredients: \nI lb fettuccine pasta \n1/2 cup butter \n1 cup grated parmesan cheese \nFresh parsley (optional)");
+console.log("\nInstructions:\n1. cook the noodles til al dente. Drain and set aside \n2. In a large skillet, melt the butter over medium heat. \n3. Stir in parmesan cheese. \n4. add cooked noodles to sauce and toss to coat evenly. \n5. Serve and add parlsey if you want.");
+
+//Secret Ingredient: use heavy whipping cream to make it even creamier!
